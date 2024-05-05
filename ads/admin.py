366fc2a9a7822214dddbeaf3fca769a4c93af24e
twist <twistysmig@mail.ru>
2024-05-05 13:45:22 +0300

    requirements needs and ads admin

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from ads.models import Ad, Review
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'description', 'author', 'created_at', 'image')
+    search_fields = ('title', 'price', 'author', )
+    list_filter = ('author', 'created_at')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'author', 'ad', 'created_at')
+    search_fields = ('id', 'author', 'ad', )
+    list_filter = ('author', 'created_at', )
